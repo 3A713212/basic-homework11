7b22e0a1b81e84b0250b8120efd88ee93367f611
3A713212 <48340382+3A713212@users.noreply.github.com>
2019-04-12 08:49:51 +0800

    Add files via upload

---
+++ hw11.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int x,y,z;
+	printf("輸入三個數: ");
+	scanf("%d%d%d",&x,&y,&z);
+	int max,mid,min;
+	if(x>y&&y>z&&x>z){
+	max=x,mid=y,min=z;
+	}
+	if(y>x&&x>z&&y>z){
+	max=y,mid=x,min=z;
+    }
+    if(z>x&&x>y&&z>y){
+    max=z,mid=x,min=y;
+	}
+	if(z>y&&y>x&&z>x){
+	max=z,mid=y,min,x;
+	}
+	if(x>z&&z>y&&x>z){
+	max=x,mid=z,min=y;
+	}
+	if(y>z&&z>x&&y>x){
+	max=y,mid=z,min=x;
+	}
+	
+	
+	printf("%d>%d>%d",max,mid,min);
+	
+ } 
